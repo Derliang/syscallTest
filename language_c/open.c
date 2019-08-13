72dba39a046c9487cc99822c8bf24d8efe48b98b
Derliang <546387449@qq.com>
2019-08-13 08:42:23 +0800

    backup

---
+++ language_c/open.c
@@ -0,0 +1,21 @@
+//
+// Created by liangwr on 19-8-13.
+//
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+
+void opentTest(char* fileName, int flag, int mode) {
+
+}
+
+void openTestAllCase() {
+
+
+}
+
+int main() {
+    openTestAllCase();
+}
